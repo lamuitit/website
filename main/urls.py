a5c88205a45dc6064ba4689885a504bc56b769f8
Lê Minh Huy <ngocnguyen.nnnc26@gmail.com>
2023-04-17 23:54:14 +0700

    update 17/04/2023
    change nav

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,17 @@ path('', views.index, name='index'),
 path('/', views.index, name='index'),
 path('home/', views.index, name='home'),
 path('camera/', views.camera, name='camera'),
+<<<<<<< Updated upstream
+=======
+path('dslr/', views.dslr, name='dslr'),
+path('mirrorless/', views.mirrorless, name='mirrorless'),
+path('login/', views.login, name='login'),
+path('canon750D', views.canon750D, name='canon750D'),
+path('introduction', views.introduction, name='introduction'),
+path('contact', views.contact, name='contact'),
+path('container', views.container, name='container'),
+path('landscape', views.landscape, name='landscape'),
+path('t', views.t, name='t'),
+>>>>>>> Stashed changes
 #path('huongdan', views.huongdan, name='huongdan'),
 ]
